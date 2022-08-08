0efdd9d636c9b6e17e26fc8f8c90ff433c8e5976
MartaKrawczy <martaakrawczykowska@gmail.com>
2022-08-08 14:16:37 +0200

    add Solution to javaSubstring

--- src/main/java/javaSubstring/Solution.java
+++ src/main/java/javaSubstring/Solution.java
@@ -1,10 +1,21 @@
 package javaSubstring;
 
+import java.util.*;
+
 public class Solution {
 
-    public static void main(String args[] ) throws Exception {
+    public static void substring(String x, int a,int b){
+        System.out.println(x.substring(a,b));
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
 
+        substring(S,start,end);
 
     }
-}
+
+    }
 
