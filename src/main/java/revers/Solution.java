90122e15626a8760a9839f4dd5e76e1ce4002636
MartaKrawczy <martaakrawczykowska@gmail.com>
2022-08-23 15:27:48 +0200

    anagram

--- src/main/java/revers/Solution.java
+++ src/main/java/revers/Solution.java
@@ -1,2 +1,22 @@
-package revers;public class Solution {
+package revers;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        System.out.println(Revers(A));
+    }
+
+    public static String Revers(String a){
+        StringBuffer b = new StringBuffer(a);
+        String c = b.reverse().toString();
+       if( c.equals(a))
+       {
+           return "Yes";
+       } else
+        return "No";
+    }
 }
